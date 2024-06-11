96a9ee0918ce63e39d44ff91e19ef250dd0477dd
Shyam panchasara <141407305+Shyam0003@users.noreply.github.com>
2024-06-11 11:22:20 +0530

    Update Products.jsx

--- React/Components/Products.jsx
+++ React/Components/Products.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
+import React from 'react';
 
-function Products({name,data}) {
+function Products({ name, age, sname }) {
   return (
     <div className='w-full h-screen bg-black text-slate-200'>
-        <h1>{name}</h1>
-        <h1>{data.age}</h1>
-        <h1>{data.sname}</h1>
+      <h1>{name}</h1>
+      <h1>{age}</h1>
+      <h1>{sname}</h1>
     </div>
-
-  )
+  );
 }
 
-export default Products
+export default Products;
